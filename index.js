6659967a0e7916edf3c2ef463e1340fd873c3ce6
Miguelangel Diaz <Miguel.Diaz@jkmoving.com>
2023-03-13 14:37:34 -0500

    46. Passport Callbacks.

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ const express = require("express");
 const mongoose = require("mongoose");
 
 require("./models/User");
-require("./services/passport");
+const passport = require("./services/passport");
 const authRoutes = require("./routes/authRoutes");
 const { mongoURI } = require("./config/keys");
 
 mongoose.connect(mongoURI);
 const app = express();
 app.set("trust proxy", 1);
+passport(app);
 authRoutes(app);
 // require("./routes/authRoutes")(app)  // alternative way to import and use authRoutes in a single line
 
--- services/passport.js
+++ services/passport.js
@@ -7,26 +7,33 @@ const { googleClientID, googleClientSecret } = require("../config/keys");
 const User = mongoose.model("users");
 
 // console.developers.google.com
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: googleClientID,
-      clientSecret: googleClientSecret,
-      callbackURL: "http://localhost:5000/auth/google/callback",
-      userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
-    },
-    async (accessToken, refreshToken, profile, done) => {
-      console.log("accessToken:", accessToken);
-      console.log("refreshToken:", refreshToken);
-      console.log("profile:", profile);
 
-      const userExist = await User.findOne({ googleId: profile.id });
+module.exports = (app) => {
+  passport.use(
+    new GoogleStrategy(
+      {
+        clientID: googleClientID,
+        clientSecret: googleClientSecret,
+        callbackURL: "http://localhost:5000/auth/google/callback",
+        userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
+      },
+      async (accessToken, refreshToken, profile, done) => {
+        console.log("accessToken:", accessToken);
+        console.log("refreshToken:", refreshToken);
+        console.log("profile:", profile);
 
-      if (!userExist) {
-        new User({ googleId: profile.id }).save();
-      } else {
-        console.log("This user is created already!");
+        const user = await User.findOne({ googleId: profile.id });
+
+        if (!user) {
+          const newUser = await new User({ googleId: profile.id })
+            .save()
+            .then((user) => done(null, user));
+        } else {
+          console.log("This user is created already!");
+          done(null, user);
+        }
       }
-    }
-  )
-);
+    )
+  );
+  app.use(passport.initialize());
+};
