45bd073b53a921721bf2b9482375fce8ef74a456
Miguelangel Diaz <Miguel.Diaz@jkmoving.com>
2023-03-13 08:32:58 -0500

    30.-33. Nodemon setup

--- index.js
+++ index.js
@@ -5,16 +5,20 @@ const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const { googleClientID, googleClientSecret } = require("./config/keys");
 
 const app = express();
+app.set("trust proxy", 1);
 // console.developers.google.com
 passport.use(
   new GoogleStrategy(
     {
       clientID: googleClientID,
       clientSecret: googleClientSecret,
-      callbackURL: "/auth/google/callback",
+      callbackURL: "http://localhost:5000/auth/google/callback",
+      userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
     },
-    (accessToken) => {
-      console.log(accessToken);
+    (accessToken, refreshToken, profile, done) => {
+      console.log("accessToken:", accessToken);
+      console.log("refreshToken:", refreshToken);
+      console.log("profile:", profile);
     }
   )
 );
@@ -25,6 +29,8 @@ app.get(
     scope: ["profile", "email"],
   })
 );
+
+app.get("/auth/google/callback", passport.authenticate("google"));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
