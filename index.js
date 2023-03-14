01a27c0ef08a4de21a7d9b82e187051f0660c847
Miguelangel Diaz <Miguel.Diaz@jkmoving.com>
2023-03-14 14:30:50 -0500

    52. Login Out Users

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cookieSession = require("cookie-session");
-const passport = require("passport"); // This simpler way of solving the previous initialize error, buy I'll stick with the way I found
+// const passport = require("passport"); // This simpler way of solving the previous initialize error, buy I'll stick with the way I found
 
 require("./models/User");
 const passportService = require("./services/passport");
@@ -25,5 +25,8 @@ passportService(app);
 // require("./routes/authRoutes")(app)  // alternative way to import and use authRoutes in a single line
 authRoutes(app);
 
+// process.env.[ENV_VARIABLE_NAME] will only work on production meaning that you need a special
+// library loader (npm package like dotenv) to load environment variables on a dev environment.
 const PORT = process.env.PORT || 5000;
+
 app.listen(PORT);
